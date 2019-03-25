dd77c975df7ae00e35b2caac10a087ca91bdba4d
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-03-25 11:33:29 -0400

    Added base json parser

---
+++ src/Urbania/AppleNews/Support/JsonParser.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Urbania\AppleNews\Support;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\ClientException;
+use Urbania\AppleNews\Article;
+
+abstract class JsonParser extends Parser
+{
+    protected $client;
+    protected $htmlParser;
+
+    protected $article = [];
+
+    public function __construct($opts = [])
+    {
+        $this->setOptions($opts);
+
+        $this->client = new HttpClient();
+    }
+
+    public function setOptions(array $opts)
+    {
+        if (isset($opts['article'])) {
+            $this->article = $opts['article'];
+        }
+    }
+
+    abstract public function parse($post, $article = null);
+
+    public function fetchJson($url, $method = 'GET', $opts = [])
+    {
+        try {
+            $response = $this->client->request($method, $url, $opts);
+            return json_decode((string)$response->getBody(), true);
+        } catch (ClientException $e) {
+            return null;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+}
