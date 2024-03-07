8e4e652fd9efc2ccd432c11f5093ee7f8eb95ab6
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-03-06 20:22:48 -0500

    Fix

--- src/Urbania/AppleNews/Contracts/Api.php
+++ src/Urbania/AppleNews/Contracts/Api.php
@@ -10,4 +10,18 @@ interface Api
      * @return \Urbania\AppleNews\Api\ChannelsClient
      */
     public function channels($channelId = null);
+
+    /**
+     * Get the articles API Client
+     * @param string|null $channelId The channel id
+     * @return \Urbania\AppleNews\Api\ArticlesClient
+     */
+    public function articles($channelId = null);
+
+    /**
+     * Get the sections API Client
+     * @param string|null $channelId The channel id
+     * @return \Urbania\AppleNews\Api\SectionsClient
+     */
+    public function sections($channelId = null);
 }
